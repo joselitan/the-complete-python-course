da46eeadb80a3c2f527004ee0f2d7b31ba1211fa
JoseMartin <joselitan@gmail.com>
2022-12-22 09:01:13 +0100

    updating test.py file with some random for loops and functions.

--- section_2/test.py
+++ section_2/test.py
@@ -1,28 +1,21 @@
-friend_ages = {"Rolf": 25, "Anne": 37, "Charlie": 31, "Bob": 22}
-
-#for name in friend_ages.values():
-#    #print(f'my friends name is {name} and is {age} years old')
-#    print(f'age of friend: {name}')
-
-for name, age in friend_ages.items():
-    #print(f'my friends name is {name} and is {age} years old')
-    #print(f'{name} is {age} years old.'
-    pass
 
 movies = [{"title": "Avengers", "director": "Steve Rogers", "year": "2010"},
           {"title": "Iron Man", "director": "Robert Downey Jr", "year": "2008"},
           {"title": "Thor", "director": "Chris Hemworth", "year": "2012"}]
 
-for i in range(0, len(movies)):
-
-    title = movies[i]["title"]
-    director = movies[i]["director"]
-    year = movies[i]["year"]
-    search_movie = input(f'enter title for the movie you are looking for: ')
-    if search_movie == title:
-        print(f"""your movie {title} was found and was directed by {director} in {year}""")
-        pass
-
+# for i in range(0, len(movies)):
+#
+#     title = movies[i]["title"]
+#     director = movies[i]["director"]
+#     year = movies[i]["year"]
+#     search_movie = input(f'enter title for the movie you are looking for: ')
+#     if search_movie == title:
+#         print(f"""your movie {title} was found and was directed by {director} in {year}""")
+#         pass
+res = None
+search_title = input("Enter the title for your movie: ")
 for movie in movies:
-    print(movie)
-#print(movies)
+    if movie['title'] == search_title:
+        res = movie
+        break
+print("The title was found: " + str(res))
